579e9eb489e38bc8dfa2a8ddeba3c071f146972d
David Lawrence <davmlaw@gmail.com>
2022-02-14 17:50:50 +1030

    Use newer Django cache backend

--- variantgrid/settings/components/default_settings.py
+++ variantgrid/settings/components/default_settings.py
@@ -98,13 +98,13 @@ REDIS_PORT = 6379
 CACHE_VERSION = 31  # increment to flush caches (eg if invalid due to upgrade)
 CACHES = {
     'default': {
-        "BACKEND": "redis_cache.RedisCache",
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": "redis://127.0.0.1:%d/1" % REDIS_PORT,
         'TIMEOUT': TIMEOUT,
         'VERSION': CACHE_VERSION,
     },
     'debug-panel': {
-        "BACKEND": "redis_cache.RedisCache",
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": "redis://127.0.0.1:%d/1" % REDIS_PORT,
         'TIMEOUT': TIMEOUT,
         'OPTIONS': {
